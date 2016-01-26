6a447c86e1e936135e961ae8a187f00b01f43ef8
sgibbes <sgibbes@wri.org>
2016-01-26 11:02:33 -0500

    returned hotspot to orginal state

--- hotspots.py
+++ hotspots.py
@@ -30,37 +30,37 @@ arcpy.env.workspace = outdir
 arcpy.env.snapRaster = hansenareamosaic
 
 #'''Section 5: main body of script ############################################################################'''
-#change buffer distance to suit aoi of script.
-# print "     buffering aoi"
-# f = os.path.basename(indir).split(".")[0]
-# outbuff =os.path.join(maindir,f+"_buff.shp")
-# arcpy.Buffer_analysis(indir, outbuff, '3 Kilometers')
-#
-# print "     extracting by mask"
-# outExtractbyMask = ExtractByMask(lossyearmosaic,outbuff)
-#
-# print "     multiplying"
-# outMult =outExtractbyMask*Raster(tcdmosaic)
-#
-# print "     converting to point"
-# outpoint = os.path.join(maindir,f+"_hs_points.shp")
-# arcpy.RasterToPoint_conversion(outMult, outpoint, "VALUE")
-#
-# print "     projecting"
-# outprj = os.path.join(maindir,f+"_hs_points_prj.shp")
-# out_coordinate_system = arcpy.SpatialReference('Eckert IV (world)')
-# arcpy.Project_management(outpoint, outprj, out_coordinate_system)
-#
-# print "     adding columns"
-# arcpy.AddField_management(outprj, "date", "DATE")
-#
-# print "     calculating date"
-# layer = arcpy.MakeFeatureLayer_management(outprj, "aoi_lyr")
-# arcpy.SelectLayerByAttribute_management(layer,"NEW_SELECTION", "GRID_CODE <10")
-# arcpy.CalculateField_management(layer, field="date", expression=""""1/1/200"+str(!GRID_CODE!)""", expression_type="PYTHON_9.3", code_block="")
-#
-# arcpy.SelectLayerByAttribute_management(layer,"NEW_SELECTION", "GRID_CODE >=10")
-# arcpy.CalculateField_management(layer, field="date", expression=""""1/1/20"+str(!GRID_CODE!)""", expression_type="PYTHON_9.3", code_block="")
+change buffer distance to suit aoi of script.
+print "     buffering aoi"
+f = os.path.basename(indir).split(".")[0]
+outbuff =os.path.join(maindir,f+"_buff.shp")
+arcpy.Buffer_analysis(indir, outbuff, '3 Kilometers')
+
+print "     extracting by mask"
+outExtractbyMask = ExtractByMask(lossyearmosaic,outbuff)
+
+print "     multiplying"
+outMult =outExtractbyMask*Raster(tcdmosaic)
+
+print "     converting to point"
+outpoint = os.path.join(maindir,f+"_hs_points.shp")
+arcpy.RasterToPoint_conversion(outMult, outpoint, "VALUE")
+
+print "     projecting"
+outprj = os.path.join(maindir,f+"_hs_points_prj.shp")
+out_coordinate_system = arcpy.SpatialReference('Eckert IV (world)')
+arcpy.Project_management(outpoint, outprj, out_coordinate_system)
+
+print "     adding columns"
+arcpy.AddField_management(outprj, "date", "DATE")
+
+print "     calculating date"
+layer = arcpy.MakeFeatureLayer_management(outprj, "aoi_lyr")
+arcpy.SelectLayerByAttribute_management(layer,"NEW_SELECTION", "GRID_CODE <10")
+arcpy.CalculateField_management(layer, field="date", expression=""""1/1/200"+str(!GRID_CODE!)""", expression_type="PYTHON_9.3", code_block="")
+
+arcpy.SelectLayerByAttribute_management(layer,"NEW_SELECTION", "GRID_CODE >=10")
+arcpy.CalculateField_management(layer, field="date", expression=""""1/1/20"+str(!GRID_CODE!)""", expression_type="PYTHON_9.3", code_block="")
 
 
 print "     create space time cuuuube"
