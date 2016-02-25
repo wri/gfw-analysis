eea6f40cea663e4e0a6fb50368519607f18b70bf
sgibbes <sgibbes@wri.org>
2016-02-25 11:26:56 -0500

    trying to get text decoded
    works in stand alone script, throws error in tool...

--- forest_loss_analysis_tool_simple.py
+++ forest_loss_analysis_tool_simple.py
@@ -1,4 +1,5 @@
-__author__ = 'sgibbes'
+#!/usr/bin/python
+# This Python file uses the following encoding: utf-8
 import os
 import arcpy
 from arcpy import *
@@ -48,12 +49,12 @@ def zonal_stats_forest(zone_raster, value_raster, filename, calculation, snapras
     if calculation == "area_30m" or calculation == "biomass_30m":
         arcpy.env.cellSize = "MAXOF"
 
-    if " " in column_name:
-        column_name = column_name.replace(" ","_")
-    if column_name[0].isdigit():
-        column_name = "x"+str(column_name)
+    # if " " in column_name:
+    #     column_name = column_name.replace(" ","_")
+    # if column_name[0].isdigit():
+    #     column_name = "x"+str(column_name)
 
-    z_stats_tbl = os.path.join(outdir, column_name + "_" + filename + "_" + calculation)
+    z_stats_tbl = os.path.join(outdir, column_name2 + "_" + filename + "_" + calculation)
 
     arcpy.gp.ZonalStatisticsAsTable_sa(zone_raster, "VALUE", value_raster, z_stats_tbl, "DATA", "SUM")
     # add a field to identify the table onces it is merged with the other zonal stats tables
@@ -156,7 +157,7 @@ def pivotTable(input_table,field,fname):
 def loss_and_biomass(option):
     arcpy.env.scratchWorkspace = scratch_gdb
     table_names_27m = ["area","biomass_max","biomass_min","area_only"]
-    z_stats_tbl = os.path.join(outdir, column_name + "_" + filename + "_" + table_names_27m[0])
+    z_stats_tbl = os.path.join(outdir, column_name2 + "_" + filename + "_" + table_names_27m[0])
     if not os.path.exists(z_stats_tbl):
         arcpy.env.snapRaster = hansenareamosaic
         try:
@@ -241,12 +242,15 @@ def user_inputs():
 #--------------------------
 
 # set input files
-hansenareamosaic = arcpy.GetParameterAsText(7)
-biomassmosaic = arcpy.GetParameterAsText(8)
-tcdmosaic30m = arcpy.GetParameterAsText(9)
-hansenareamosaic30m = arcpy.GetParameterAsText(10)
-lossyearmosaic = arcpy.GetParameterAsText(11)
-tcdmosaic = arcpy.GetParameterAsText(12)
+lossyearmosaic = arcpy.GetParameterAsText(7)
+tcdmosaic = arcpy.GetParameterAsText(8)
+hansenareamosaic = arcpy.GetParameterAsText(9)
+
+biomassmosaic = arcpy.GetParameterAsText(10)
+tcdmosaic30m = arcpy.GetParameterAsText(11)
+hansenareamosaic30m = arcpy.GetParameterAsText(12)
+
+
 overwrite = arcpy.GetParameterAsText(13)
 
 remaptable = os.path.join(os.path.dirname(os.path.abspath(__file__)),"remaptable.dbf")
@@ -255,7 +259,7 @@ remapfunction = os.path.join(os.path.dirname(os.path.abspath(__file__)),"remapfu
 maindir, shapefile, column_name,main_analysis,biomass_analysis,filename,threshold = user_inputs()
 
 # remap tcd mosaic based on user input
-remapmosaic(threshold,remaptable,remapfunction)
+# remapmosaic(threshold,remaptable,remapfunction)
 
 # set up directories
 arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("WGS 1984")
@@ -279,6 +283,14 @@ if not os.path.exists(merged_dir):
 total_features = int(arcpy.GetCount_management(shapefile).getOutput(0))
 start = datetime.datetime.now()
 option_list = []
+def correctfcname(column_name):
+    # column_name = unicode(column_name,"ascii","ignore")
+    if column_name[0].isdigit():
+        column_name = "x"+str(column_name)
+    bad = [" ","-",'/', ':', '*', '?', '"', '<', '>', '|']
+    for char in bad:
+        column_name = column_name.replace(char,"_")
+    return  column_name
 
 with arcpy.da.SearchCursor(shapefile, ("Shape@", column_name)) as cursor:
     feature_count = 0
@@ -287,12 +299,10 @@ with arcpy.da.SearchCursor(shapefile, ("Shape@", column_name)) as cursor:
         feature_count += 1
         fc_geo = row[0]
 
-        column_name = str(row[1])
-        column_name2 = str(row[1])
-        if " " in column_name:
-            column_name2 = column_name.replace(" ","_")
-        if column_name[0].isdigit():
-            column_name2 = "x"+str(column_name)
+        column_name = unicode(column_name,"ascii","ignore")
+        # column_name = correctfcname(str(row[1]))
+        # column_name2 = correctfcname(column_name)
+        arcpy.AddMessage(column_name2)
         # skip loss and biomass if area file exists
         table_names = {"area only":"area_only","loss and biomass":"area","biomass":"biomass30m","loss":"area"}
         arcpy.AddMessage(overwrite)
@@ -301,7 +311,7 @@ with arcpy.da.SearchCursor(shapefile, ("Shape@", column_name)) as cursor:
             if main_analysis != "none":
                 analysisfilename = table_names[main_analysis]
                 z_stats_tbl = os.path.join(outdir, column_name2 + "_" + filename + "_" + analysisfilename)
-
+                arcpy.AddMessage(z_stats_tbl)
                 if not arcpy.Exists(z_stats_tbl):
                     loss_and_biomass(main_analysis)
                 else:
